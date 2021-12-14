82b70467fe6f10d87227749de200a9119ce28bad
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-12-14 13:53:55 -0800

    Add Mango CRUD routes, and view and urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views.users import SignUp, SignIn, SignOut, ChangePassword
+from .views.mangos import MangosView, MangoView
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
-    path('change-password/', ChangePassword.as_view(), name='change-password')
+    path('change-password/', ChangePassword.as_view(), name='change-password'),
+    path('mangos/', MangosView.as_view(), name='mangos'),
+    path('mangos/<int:pk>', MangoView.as_view(), name='mango'),
 ]
---
+++ api/views/mangos.py
@@ -0,0 +1,54 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+
+from ..serializers.mango import MangoSerializer
+from ..models.mango import Mango
+from django.core.exceptions import PermissionDenied 
+
+class MangosView(APIView):
+    def post(self, request):
+        # Add the user id as owner
+        request.data['owner'] = request.user.id
+        mango = MangoSerializer(data=request.data)
+        if mango.is_valid():
+            mango.save()
+            return Response(mango.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(mango.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+    def get(self, request):
+        # filter for mangos with our user id
+        mangos = Mango.objects.filter(owner=request.user.id)
+        data = MangoSerializer(mangos, many=True).data
+        return Response(data)
+
+class MangoView(APIView):
+    def delete(self, request, pk):
+        mango = get_object_or_404(Mango, pk=pk)
+        # Check the mango's owner against the user making this request
+        if request.user != mango.owner:
+            raise PermissionDenied('Unauthorized, you do not own this mango')
+        mango.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def get(self, request, pk):
+        mango = get_object_or_404(Mango, pk=pk)
+        if request.user != mango.owner:
+            raise PermissionDenied('Unauthorized, you do not own this mango')
+        data = MangoSerializer(mango).data
+        return Response(data)
+    
+    def patch(self, request, pk):
+        mango = get_object_or_404(Mango, pk=pk)
+        # Check the mango's owner against the user making this request
+        if request.user != mango.owner:
+            raise PermissionDenied('Unauthorized, you do not own this mango')
+        # Ensure the owner field is set to the current user's ID
+        request.data['owner'] = request.user.id
+        updated_mango = MangoSerializer(mango, data=request.data)
+        if updated_mango.is_valid():
+            updated_mango.save()
+            return Response(updated_mango.data)
+        return Response(updated_mango.errors, status=status.HTTP_400_BAD_REQUEST)
