f4f77092343bb139f69c9b1c769022cc8f4e8691
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-12-14 10:45:21 -0800

    Add signout and change password views and urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views.users import SignUp, SignIn
+from .views.users import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
-    path('sign-in/', SignIn.as_view(), name='sign-in')
+    path('sign-in/', SignIn.as_view(), name='sign-in'),
+    path('sign-out/', SignOut.as_view(), name='sign-out'),
+    path('change-password/', ChangePassword.as_view(), name='change-password')
 ]
